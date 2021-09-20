35fd73b1e76210280881ea3f5edf67c359406efb
1aydar <90646653+1aydar@users.noreply.github.com>
2021-09-20 22:57:40 +0300

    1st

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            double amount;
+            double result_usd;
+            double result_uah;
+            double result_lira;
+            double rate_usd = 73.58;
+            double rate_uah = 2.75;
+            double rate_lira = 8.68;
+
+            Console.WriteLine("Enter amount in RUB");
+            str = Console.ReadLine();
+            amount = Convert.ToDouble(str);
+            result_usd = amount / rate_usd;
+            result_uah = amount / rate_uah;
+            result_lira = amount / rate_lira;
+            Console.ReadLine();
+            Console.WriteLine("Amount in USD:" +result_usd +" USD");
+            Console.ReadLine();
+            Console.WriteLine("Amount in UAH:" + result_uah +" UAH");
+            Console.ReadLine();
+            Console.WriteLine("Amount in TRY:" + result_lira +" TRY");
+            Console.ReadLine();
+        }
+    }
+}
